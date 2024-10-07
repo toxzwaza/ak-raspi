8ff72f8ba8ea853d5b2c2c53b1a9787791feb68f
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2024-10-07 17:06:45 +0900

    get

--- sensor/sendServer.py
+++ sensor/sendServer.py
@@ -4,13 +4,13 @@ from datetime import datetime
 def send_post_request(temperature, humidity):
     url = "http://monokanri-manage.local/api/respi"
     
-    data = {
+    params = {
         #1:電気炉 2:生型造型 3:フラン 4:中子 5:仕上げ 6:出荷検査
         'process_id': 1,
         'temperature': temperature,
         'humidity': humidity,
         'created_at': datetime.now().isoformat()
     }
-    response = requests.post(url, data=data)
+    response = requests.get(url, params=params)
     print(response)
     return response
