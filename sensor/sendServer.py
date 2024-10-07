4a4c55ceffe0a0b13fe1b4df678ebcbf4b6295db
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2024-10-07 16:57:31 +0900

    修正

--- sensor/sendServer.py
+++ sensor/sendServer.py
@@ -7,8 +7,8 @@ def send_post_request(temperature, humidity):
     data = {
         #1:電気炉 2:生型造型 3:フラン 4:中子 5:仕上げ 6:出荷検査
         'process_id': 1,
-        'temp': temperature,
-        'humi': humidity,
+        'temperature': temperature,
+        'humidity': humidity,
         'created_at': datetime.now().isoformat()
     }
     response = requests.post(url, data=data)
