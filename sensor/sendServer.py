9753614c82fd0c030197a2b835799d19c126cb33
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2024-10-08 17:24:57 +0900

    フラン用

--- sensor/sendServer.py
+++ sensor/sendServer.py
@@ -5,8 +5,8 @@ def send_post_request(temperature, humidity):
     url = "http://monokanri-manage.local/api/respi"
     
     params = {
-        #1:電気炉 2:生型造型 3:フラン 4:中子 5:仕上げ 6:出荷検査
-        'process_id': 1,
+        #1:電気炉 2:生型造型 3:フラン 4:中子 5:仕上げ 6:出荷検査 7:工場外
+        'process_id': 3,
         'temperature': temperature,
         'humidity': humidity,
         'created_at': datetime.now().isoformat()
